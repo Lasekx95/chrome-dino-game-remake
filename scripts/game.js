d4aa62e75ea937d48781b7770936e2fa80e46816
Dustin <ddemers1995@hotmail.com>
2023-03-12 10:36:26 -0700

    transfered files from website, creating standalone pj instead'

---
+++ scripts/game.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 200;
+const PLAYER_WIDTH = 88;
+const PLAYER_HEIGHT = 94;
+
+let scaleRatio = null;
+let previousTime = null;
+
+function setScreen(){
+    scaleRatio = getScaleRatio();
+    canvas.width = GAME_WIDTH * scaleRatio;
+    canvas.height = GAME_HEIGHT * scaleRatio;
+}
+
+setScreen();
+//Use setTimeout on Safari mobile rotation otherwise works fine on desktop
+window.addEventListener('resize', ()=> setTimeout(setScreen, 500));
+
+if(screen.orientation) {
+    screen.orientation.addEventListener('change', setScreen);
+}
+
+function getScaleRatio(){
+    const screenHeight = Math.min(
+        window.innerHeight, 
+        document.documentElement.clientHeight
+        );
+
+    const screenWidth = Math.min(
+        window.innerWidth,
+        document.documentElement.clientWidth
+    );
+
+// window is wider than game width
+    if(screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT)
+    {
+    return screenWidth / GAME_WIDTH
+    } else 
+    {
+        return screenHeight / GAME_HEIGHT
+    }
+}
+
+function clearSceen() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function gameLoop(currentTime) {
+    if(previousTime === null){
+        previousTime = currentTime;
+        requestAnimationFrame(gameLoop);
+        return;
+    }
+    const frameTimeDelta = currentTime - previousTime;
+    previousTime = currentTime
+    console/console.log(frameTimeDelta);
+    clearSceen();
+    console.log("gameloop");
+    requestAnimationFrame(gameLoop);
+
+}
+
+requestAnimationFrame(gameLoop);
